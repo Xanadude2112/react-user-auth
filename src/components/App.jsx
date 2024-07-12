7c847f9127d86052f00df09f8463fb2497e142a3
Tommy <tommy.azran@gmail.com>
2024-07-12 14:59:29 -0400

    add sign in skeleton

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div className="master">
+      <div className="user-interact">
+        <h2 className="sign-in">Sign In</h2>
+        <div>
+          <input type="text" name="" className="" placeholder="Name" />
+          <input type="text" name="" className="" placeholder="Email" />
+          <input type="text" name="" className="" placeholder="Password" />
+        </div>
+        <div className="buttons">
+        <button>Login</button>
+        <a href=""></a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
